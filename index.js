2f9c33eb719d068c9b72f43074b6623cbdd4430f
Artur Kot <arturkot85@gmail.com>
2017-10-10 09:59:47 +0200

    Format index.js using Prettier

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 module.exports = {
-  extends: [ 'airbnb-base', 'prettier' ],
-  plugins: [ 'prettier' ],
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
   env: { browser: true, es6: true },
   rules: {
-    "prettier/prettier": ["error", { "singleQuote": true, "trailingComma": "es5" }]
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        trailingComma: "es5"
+      }
+    ]
   },
-  parser: 'babel-eslint'
-}
+  parser: "babel-eslint"
+};
