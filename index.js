3040b1a090066072ed91b7f142ae028c1b8c117f
Artur Kot <arturkot85@gmail.com>
2017-10-10 09:27:35 +0200

    Add Prettier specific settings

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ module.exports = {
   extends: [ 'airbnb-base', 'prettier' ],
   plugins: [ 'prettier' ],
   env: { browser: true, es6: true },
+  rules: {
+    "prettier/prettier": ["error", { "singleQuote": true, "trailingComma": "es5" }]
+  },
   parser: 'babel-eslint'
 }
